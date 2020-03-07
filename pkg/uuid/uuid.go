b07c8c1d2fe5567a35104b76aec9e53a629bbfed
michael king <michael@chaos.haus>
2020-03-07 09:48:54 -0500

    feat(uuid): Re-use FromSubDomain

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -19,20 +19,20 @@ func New(nameSpace uuid.UUID, value string) UUID {
 	return UUID(nameSpacedId)
 }
 
-func FromAPIURL(apiUrl string) UUID     { return New(APIURLNameSpace, apiUrl) }
-func FromSubDomain(newHost string) UUID { return New(DNSNameSpace, newHost) }
-func (u UUID) String() string           { return uuid.UUID(u).String() }
+func FromAPIURL(apiUrl string) UUID                   { return New(APIURLNameSpace, apiUrl) }
+func FromSubDomain(id uuid.UUID, newHost string) UUID { return New(id, newHost) }
+func (u UUID) String() string                         { return uuid.UUID(u).String() }
 func (u UUID) Validate(nameSpace uuid.UUID, value string) bool {
 	switch nameSpace {
 	case DNSNameSpace:
 		if strings.HasSuffix(value, ".modding.engineer") {
 			subDomain := strings.TrimSuffix(value, ".modding.engineers")
-			return u.String() == FromSubDomain(subDomain).String()
+			return u.String() == FromSubDomain(DNSNameSpace, subDomain).String()
 		}
 	case APIDNSNameSpace:
 		if strings.HasSuffix(value, ".api.modding.engineer") {
 			subDomain := strings.TrimSuffix(value, ".api.modding.engineer")
-			return u.String() == New(APIDNSNameSpace, subDomain).String()
+			return u.String() == FromSubDomain(APIDNSNameSpace, subDomain).String()
 		}
 	case APIURLNameSpace:
 		u, err := url.Parse(value)
